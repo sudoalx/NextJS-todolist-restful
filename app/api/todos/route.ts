aebc2f326f26cc3a9555de54153dcca524bc516c
Alex <alex@alex-dev.me>
2024-01-28 23:46:05 -0600

    Add GET route for fetching todos

---
+++ app/api/todos/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = +(searchParams.get("take") ?? "10");
+  const skip = +(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      {
+        message: "Take must be a number",
+      },
+      { status: 400 }
+    );
+  }
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      {
+        message: "Skip must be a number",
+      },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  });
+  return NextResponse.json(todos);
+}
