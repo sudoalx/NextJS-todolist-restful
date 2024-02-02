bb820ba5e9e4b139eb34010569abfd4d20ebfdc5
Alex <alex@alex-dev.me>
2024-02-02 02:28:30 -0600

    Add updateTodo function to handle updating a todo item

---
+++ app/todos/helpers/todos.ts
@@ -0,0 +1,21 @@
+"use client";
+import { Todo } from "@prisma/client";
+
+export const updateTodo = async (
+  id: string,
+  complete: boolean
+): Promise<Todo> => {
+  const body = { complete };
+
+  const todo = await fetch(`/api/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json());
+
+  console.log(todo);
+
+  return todo;
+};
