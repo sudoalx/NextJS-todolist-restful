0118d834b7c840513c54e277d4a8143b9812eb27
Alex <alex@alex-dev.me>
2024-02-05 18:37:08 -0600

    Refactor server actions page to use REST API for todos

--- app/dashboard/server-actions/page.tsx
+++ app/dashboard/server-actions/page.tsx
@@ -1,7 +1,23 @@
-export default function ServerActionsPage() {
+import { TodosGrid } from "@/app/todos";
+import { NewTodo } from "@/app/todos/components/NewTodo";
+import prisma from "@/lib/prisma";
+
+export const metadata = {
+  title: "Server Actions Todos Page",
+  description: "Rest Todos Page",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({
+    orderBy: {
+      description: "asc",
+    },
+  });
+
   return (
     <div>
-      <h1>Server Actions Page</h1>
+      <NewTodo />
+      <TodosGrid todos={todos} />
     </div>
   );
 }
