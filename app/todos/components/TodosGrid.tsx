1485199af281314941e8c2c84825d448d2614468
Alex <alex@alex-dev.me>
2024-02-05 19:31:07 -0600

    Update TodosGrid heading

--- app/todos/components/TodosGrid.tsx
+++ app/todos/components/TodosGrid.tsx
@@ -11,7 +11,7 @@ export const TodosGrid = ({ todos }: TodosGridProps) => {
   return (
     <>
       <h1 className="text-3xl font-bold text-center">Todos</h1>
-      <h2 className="text-xl font-bold text-center">Rest Todos</h2>
+      <h2 className="text-xl font-bold text-center">Server Actions Todos</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
         {todos?.map((todo) => (
           <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
