026c86579b185103293db19bc925cf1983126848
Alex <alex@alex-dev.me>
2024-02-05 22:03:28 -0600

    Add optimistic UI for toggling todo item

--- app/todos/components/TodoItem.tsx
+++ app/todos/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { startTransition, useOptimistic } from "react";
 import { Todo } from "@prisma/client";
 import styles from "./TodoItem.module.css";
 import { IoCheckbox, IoSquareOutline } from "react-icons/io5";
@@ -9,21 +10,38 @@ interface TodoItemProps {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
+  const [todoOptimistic, toggleTodoOptimistic] = useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean) => ({
+      ...state,
+      complete: newCompleteValue,
+    })
+  );
+
+  const onToggleTodo = async () => {
+    try {
+      startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete));
+      await toggleTodo(todoOptimistic.id, !todoOptimistic.complete);
+    } catch (error) {
+      startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete));
+    }
+  };
+
   return (
     <button
-      onClick={() => toggleTodo(todo.id, !todo.complete)}
-      key={todo.id}
+      onClick={onToggleTodo}
+      key={todoOptimistic.id}
       className={`${styles.todoItem} ${
-        todo.complete ? styles.completed : styles.pending
+        todoOptimistic.complete ? styles.completed : styles.pending
       }`}
     >
-      {todo.complete ? (
+      {todoOptimistic.complete ? (
         <IoCheckbox className="text-2xl mr-2" />
       ) : (
         <IoSquareOutline className="text-2xl mr-2" />
       )}
 
-      <h3 className="text-lg font-bold">{todo.description}</h3>
+      <h3 className="text-lg font-bold">{todoOptimistic.description}</h3>
     </button>
   );
 };
