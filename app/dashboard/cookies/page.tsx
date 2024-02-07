5029b7c9705ae368080417071bb470bb2c1bae17
Alex <alex@alex-dev.me>
2024-02-07 10:20:46 -0600

    Add cookieTab variable to set the current tab in TabBar

--- app/dashboard/cookies/page.tsx
+++ app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/app/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies Page",
@@ -6,11 +7,14 @@ export const metadata = {
 };
 
 export default function CookiesPage() {
+  const cookieStore = cookies();
+  const cookieTab = cookieStore.get("currentTab")?.value ?? 1;
+
   return (
     <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={+cookieTab} />
       </div>
     </div>
   );
