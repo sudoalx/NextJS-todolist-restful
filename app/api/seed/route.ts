ca30fe1eda670f8b2817e474a764e84f2e63b5ec
Alex <alex@alex-dev.me>
2024-02-08 16:06:00 -0600

    Add user creation and todo seeding

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -3,35 +3,47 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.todo.createMany({
-    data: [
-      {
-        description: "Collect Mind Stone",
-        complete: true,
+  const user = await prisma.user.create({
+    data: {
+      email: "test@sudoalex.dev",
+      name: "Alex",
+      password: "password",
+      roles: ["admin", "user", "editor"],
+      todos: {
+        create: [
+          {
+            description: "Collect Soul Stone",
+            complete: false,
+          },
+          {
+            description: "Collect Time Stone",
+            complete: true,
+          },
+          {
+            description: "Collect Space Stone",
+            complete: false,
+          },
+          {
+            description: "Collect Reality Stone",
+            complete: true,
+          },
+          {
+            description: "Collect Power Stone",
+            complete: false,
+          },
+          {
+            description: "Collect Mind Stone",
+            complete: false,
+          },
+        ],
       },
-      {
-        description: "Collect Soul Stone",
-        complete: false,
-      },
-      {
-        description: "Collect Power Stone",
-        complete: false,
-      },
-      {
-        description: "Collect Time Stone",
-        complete: false,
-      },
-      {
-        description: "Collect Reality Stone",
-        complete: true,
-      },
-      {
-        description: "Collect Space Stone",
-        complete: false,
-      },
-    ],
+    },
   });
 
-  return NextResponse.json({ message: "Seed executed!" });
+  return NextResponse.json({
+    message: "Seed executed!",
+    data: user,
+  });
 }
