81d9d294dd847049d1ef8ec09d1d3b10bd791b3b
Alex <alex@alex-dev.me>
2024-02-08 15:05:07 -0600

    Add CredentialsProvider for authentication

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -4,6 +4,7 @@ import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -15,6 +16,25 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: 1,
+          name: "J Smith",
+          email: "alex@sudoalex.dev",
+        };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 
